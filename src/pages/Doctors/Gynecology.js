b412dcd20095d2e32e8c8eced8878f398c406b80
Shafayathub <shafayat.ms@gamilcom>
2023-02-23 17:10:16 +0600

    created dev branch to experiment

--- src/pages/Doctors/Gynecology.js
+++ src/pages/Doctors/Gynecology.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React, { useEffect, useState } from 'react';
 import Doctor from '../components/Doctor';
 
@@ -12,13 +13,26 @@ const Gynecology = () => {
     doctor.speciality.toLowerCase().includes('gynecology')
   );
   return (
-    <section className="min-h-screen p-5">
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
-        {gynecologyDoctor.map((doctor) => (
-          <Doctor key={doctor._id} doctor={doctor}></Doctor>
-        ))}
-      </div>
-    </section>
+    <>
+      <Head>
+        <title>Gynecology | Doctor | Doctor Service BD</title>
+        <meta
+          name="description"
+          content="Doctor Service BD. We are committed to serve you with better Health Care. Welcome to
+              you our Doctor Service BD website to take the services and serial
+              of all expert and experienced doctors in Rajshahi."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="min-h-screen p-5">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
+          {gynecologyDoctor.map((doctor) => (
+            <Doctor key={doctor._id} doctor={doctor}></Doctor>
+          ))}
+        </div>
+      </main>
+    </>
   );
 };
 
